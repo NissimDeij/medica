40b45d99a52de1574f85443d010bbc90454f4b06
Duoc UC <Duoc UC@B33-15>
2017-12-19 14:04:23 -0300

    ND - quite el digito verificador del menuTrabajador

---
+++ consultaMedica/src/consultamedica/ConsultaMedica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consultamedica;
+
+/**
+ *
+ * @author Duoc UC
+ */
+public class ConsultaMedica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
