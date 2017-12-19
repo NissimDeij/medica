19a9124469f7f0daf338faba94f240f92cefcac8
ND <>
2017-12-19 14:24:04 -0300

    ND - conexion BD

--- consultaMedica/src/sql/conexion.java
+++ consultaMedica/src/sql/conexion.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sql;
 
-/**
- *
- * @author Duoc UC
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class conexion {
-    
+    public static Connection getConexion(){
+        
+        Connection connection = null;
+        
+        try {
+            String driverClassName = "com.mysql.jdbc.Driver";
+            String driverUrl = "jdbc:mysql://localhost/consultamedica";
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(driverUrl, "root","");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return connection;
+    }    
 }
