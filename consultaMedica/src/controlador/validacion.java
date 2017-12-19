5c0710a67bd474786af6654369120925a45613ee
Duoc UC <Duoc UC@B33-15>
2017-12-19 18:24:57 -0300

    ND - agragacion del main y correccion de detalles del controlador

--- consultaMedica/src/controlador/validacion.java
+++ consultaMedica/src/controlador/validacion.java
@@ -5,34 +5,39 @@ import sql.conexion;
 public class validacion extends conexion{
     
     public static boolean validarRut(String rut){
-        String rut1;
+        String rut1 = "";
         String rut2 = "";
         try {
-            rut = rut.toLowerCase();
+            rut = rut.toUpperCase();
             rut = rut.replace(".",""); //escapa el punto
             rut = rut.replace(",",""); //escapa la coma
             rut = rut.replace("-",""); //escapa el guion
             rut = rut.replace("_",""); //escapa el guion bajo
             rut = rut.replace(" ",""); //escapa el espacio
             
-            String dv1 = rut.substring(rut.length()); //captura como String el digito verificador
-            rut1 = rut.substring(0,rut.length()); //quitamos el digito verificador
+            String dv1 = rut.substring( rut.length()-1 ); //captura como String el digito verificador
+            System.out.println(dv1);
+            rut1 = rut.substring(0,rut.length()-1); //quitamos el digito verificador
+            System.out.println(rut1);
             
-            for (int i=1;i<=rut1.length();i++){ //para invertir el orden de los digitos del rut
-                rut2 = rut1.substring(i) + rut2;   
+            for (int i=0;i<=rut1.length()-1;i++){ //para invertir el orden de los digitos del rut
+                rut2 = rut1.charAt(i) + rut2;   
             }
+            
+            System.out.println(rut2);
             int mult=2; //multiplicador
             int suma=0; //almacenará la sumatoria
-            for (int i=1;i<=rut1.length();i++){
-                suma = suma + mult*Integer.parseInt(rut1.substring(i)); //suma cada producto entre el rut invertido y el multiplicador
-                if (rut1.length()==7){ //si se alcanzó la séptima posición resetea el multiplicador
+            for (int i=0;i<=rut1.length()-1;i++){
+                suma = suma + mult*Integer.parseInt(String.valueOf(rut1.charAt(i))); //suma cada producto entre el rut invertido y el multiplicador
+                if (i==7){ //si se alcanzó la séptima posición resetea el multiplicador
                     mult = 1; //es 1 porque 2 lineas mas abajo se le suma 1 en el m++
                 }
                 mult++;
             }
+            System.out.println(suma);
             int resto = suma%11; //determina el resto del cuociente entre suma y 11
             String dv2 = String.valueOf(11-resto); ///le sustrae el resto al 11
-            
+            System.out.println(dv2);
             if (dv2.equals("11")){
                 dv2 = "0";
             } else if (dv2.equals("10")){
